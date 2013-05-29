eac16a6efdfbb42811202303dd3eefb3aec9ca38
Mike Krisel <michaelkrisel@gmail.com>
2013-05-28 19:48:42 -0700

    simplify controller and add about content

--- htdocs/src/controller/controller.php
+++ htdocs/src/controller/controller.php
@@ -43,21 +43,7 @@ class Controller
 	{
 		if (!isset($_SESSION['username'])) {
 			if (isset($_GET['action']) && $_GET['action'] == "about") {
-				$content = '<h2>About MySQL Project</h2>
-					<p>This is the final project for WEBLAMP 443: MySQL Design, Development and Administration. Project requirements were creating a web page using PHP that works with a MySQL database. The webpage must use client side JavaScript (do some client side validation or use Ajax to get some data into the browser). The following actions/
-					components are required to be part of the project:</p>
-					<ul>
-						<li>Query/modify the database</li>
-						<li>Use transactions</li>
-						<li>Use triggers</li>
-						<li>Use functions</li>
-						<li>Use stored procedure</li>
-						<li>Use cursors</li>
-						<li>Use views</li>
-						<li>Provide debugging abilities (when sending a parameter like debug=1, add some debugging info to the webpage – queries being executed, latency of the queries…)</li>
-						<li>Provides metrics (when sending a parameter like metrics=1 shows some metrics)</li>
-					</ul>
-					<p>A repository of the project can be found at <a href="https://github.com/mikekrisel/mysql-project/tree/master/htdocs">github.com/mikekrisel/mysql-project</a></p>';
+				include "src/view/about.php";
 				include "src/view/main.php";
 			} else {
 				include "src/view/login.php";
@@ -71,21 +57,7 @@ class Controller
 			}
 			switch ($action) {
 				case "about":
-					$content = '<h2>About MySQL Project</h2>
-						<p>This is the final project for WEBLAMP 443: MySQL Design, Development and Administration. Project requirements were creating a web page using PHP that works with a MySQL database. The webpage must use client side JavaScript (do some client side validation or use Ajax to get some data into the browser). The following actions/
-						components are required to be part of the project:</p>
-						<ul>
-							<li>Query/modify the database</li>
-							<li>Use transactions</li>
-							<li>Use triggers</li>
-							<li>Use functions</li>
-							<li>Use stored procedure</li>
-							<li>Use cursors</li>
-							<li>Use views</li>
-							<li>Provide debugging abilities (when sending a parameter like debug=1, add some debugging info to the webpage – queries being executed, latency of the queries…)</li>
-							<li>Provides metrics (when sending a parameter like metrics=1 shows some metrics)</li>
-						</ul>
-						<p>A repository of the project can be found at <a href="https://github.com/mikekrisel/mysql-project/tree/master/htdocs">github.com/mikekrisel/mysql-project</a></p>';
+					include "src/view/about.php";
 					include "src/view/main.php";
 				break;
 				case "add-movie":
---
+++ htdocs/src/view/about.php
@@ -0,0 +1,19 @@
+<?php 
+
+$content = '<h2>About MySQL Project</h2>
+<p>This is the final project for WEBLAMP 443: MySQL Design, Development and Administration. Project requirements were creating a web page using PHP that works with a MySQL database. The webpage must use client side JavaScript (do some client side validation or use Ajax to get some data into the browser). The following actions/
+components are required to be part of the project:</p>
+<ul>
+	<li>Query/modify the database</li>
+	<li>Use transactions</li>
+	<li>Use triggers</li>
+	<li>Use functions</li>
+	<li>Use stored procedure</li>
+	<li>Use cursors</li>
+	<li>Use views</li>
+	<li>Provide debugging abilities (when sending a parameter like debug=1, add some debugging info to the webpage – queries being executed, latency of the queries…)</li>
+	<li>Provides metrics (when sending a parameter like metrics=1 shows some metrics)</li>
+</ul>
+<p>A repository of the project can be found at <a href="https://github.com/mikekrisel/mysql-project/tree/master/htdocs">github.com/mikekrisel/mysql-project</a></p>';
+
+?>
